299b4bcacb7f906a8323837a601aff93c354be3a
Sudhir Yadav <sudhiryadav.igi@gmail.com>
2021-10-18 03:07:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -105,7 +105,7 @@ rom_eleven(){
      repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-device,-mips,-darwin,-notdefault
      git clone https://github.com/TheSanty/local_manifests.git -b $rom .repo/local_manifests
      repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
-     . build/envsetup.sh && lunch potato_whyred-userdebug
+     source build/envsetup.sh && lunch potato_whyred-userdebug
 }
 
 recovery_one(){
@@ -232,7 +232,7 @@ case "${rom}" in
     ;;
  "aosp") m -j18 2>&1 | tee build.log
     ;;
- "POSP") m -j18 2>&1 | tee build.log
+ "POSP") brunch whyred -j18 2>&1 | tee build.log
     ;;
  "PBRP") make recoveryimage 2>&1 | tee build.log
     ;;
