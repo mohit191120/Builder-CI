dc4f0e38bfc0712695dc135e299fce49c25d3da5
Sudhir Yadav <sudhiryadav.igi@gmail.com>
2021-10-18 13:44:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -105,6 +105,7 @@ rom_eleven(){
      repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-device,-mips,-darwin,-notdefault
      git clone https://github.com/TheSanty/local_manifests.git -b $rom .repo/local_manifests
      repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
+     export ALLOW_MISSING_DEPENDENCIES=true
      source build/envsetup.sh && lunch potato_whyred-userdebug
 }
 
