93b8deb954d54fffeb297b998708860a07588bfb
mohit <85410744+mohit191120@users.noreply.github.com>
2021-10-24 01:27:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,10 @@ rom_one(){
 
 rom_two(){
      repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+     git clone https://github.com/TheSanty/local_manifests.git -b $rom .repo/local_manifests
      repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
+     git clone https://${TOKEN}@github.com/Corvus-R/.certs certs
+     export RAVEN_LAIR=Official
      . build/envsetup.sh && lunch corvus_whyred-user
 }
 
